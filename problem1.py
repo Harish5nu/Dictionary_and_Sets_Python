3da154c2b7cc53a81aec8df7476f7561a3627272
Harish Saud <saudharish770@gmail.com>
2025-06-22 14:31:55 +0545

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,11 @@
+# write a program to create a dictionary of nepali word with values as their english translation. Provide user with an option to look it up!
+words={
+    "maya":"love",
+    "pani": "water",
+    "topi": "cap"
+}
+word = input("Enter the word you want to meaning of:")
+print(words[word])
+
+# output Enter the word you want to meaning of:maya
+# love
